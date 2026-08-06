31d70140a36e50c75be46650b722693f9f9f0fff
berogoma <cca3485@gmail.com>
2026-08-07 00:44:48 +0900

    ダミーデータを作成する #3

---
+++ database/seeders/GenresSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Genre;
+use Illuminate\Database\Seeder;
+
+class GenresSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $genres = [
+            'アクション',
+            'ファンタジー',
+            'SF',
+            '恋愛',
+            '日常',
+            'ホラー',
+            'ミステリー',
+            '魔法少女',
+            'コメディ',
+            '異世界転生',
+        ];
+
+        foreach ($genres as $genre) {
+            Genre::create([
+                'name' => $genre,
+            ]);
+        }
+    }
+}
